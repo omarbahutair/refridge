101fb1bf7c90f2e57bbeafb824635dc5ab3b560c
GhalebAldoboni <116922578+GhalebAldoboni@users.noreply.github.com>
2024-05-31 19:48:08 +0400

    Update Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="footer">
-      <p>&copy; 2024 ReFridge. All rights reserved.</p>
+      <p>&copy; 2024 ReFridge. Designed and Coded by Ahmed, Ghaleb and Omar.</p>
     </div>
   );
 };
